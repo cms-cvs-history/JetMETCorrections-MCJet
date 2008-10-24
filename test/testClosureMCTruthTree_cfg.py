ae814484844e77867eddcc84078aa84860f39415
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2008-10-24 22:18:37 +0000

    Update code

--- test/testClosureMCTruthTree_cfg.py
+++ test/testClosureMCTruthTree_cfg.py
@@ -7,9 +7,9 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
 #############   Define the source file ###############
+#process.load("JetMETCorrections.MCJet.QCDDiJet_50_80_cfi")
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-'/store/relval/CMSSW_2_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V6_10TeV_v7/0000/044A7A03-BE6E-DD11-B3C0-000423D98B08.root')
+    fileNames = cms.untracked.vstring('/store/mc/Summer08/QCDDiJetPt50to80/GEN-SIM-RECO/IDEAL_V9_v3/0001/0079B347-9E9A-DD11-ADD9-003048C17FC0.root')
 )
 #############   Include the jet corrections ##########
 process.load("JetMETCorrections.Configuration.L2L3Corrections_iCSA08_S156_cff")
@@ -19,13 +19,13 @@ process.prefer("L2L3JetCorrectorIcone5")
 process.caloMctruthTree = cms.EDAnalyzer("CaloMCTruthTreeProducer",
     jets               = cms.string('L2L3CorJetIcone5'),
     genjets            = cms.string('iterativeCone5GenJets'),
-    histogramFile      = cms.string('caloClosureMCTruthTree.root')
+    histogramFile      = cms.string('ic05CaloClosureMctruthTree.root')
 )
 #############    PFJets   ############################
 process.pfMctruthTree = cms.EDAnalyzer("PFMCTruthTreeProducer",
     jets               = cms.string('L2L3CorJetPFIcone5'),
     genjets            = cms.string('iterativeCone5GenJets'),
-    histogramFile      = cms.string('pfClosureMCTruthTree.root')
+    histogramFile      = cms.string('ic05PFClosureMctruthTree.root')
 )
 #############   Path       ###########################
 process.p = cms.Path(process.L2L3CorJetIcone5 * process.caloMctruthTree * process.L2L3CorJetPFIcone5 * process.pfMctruthTree)
